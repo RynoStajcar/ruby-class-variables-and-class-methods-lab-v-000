cffd31f0b61876f87b6fa2f3693f7b9ea2be392b
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-17 15:52:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,12 @@ class Song
 @@song_count = 0
 
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
+    @name = name,
+    @artist = artist,
     @genre = genre
   end
 
   def name
-    
+
 
 end
