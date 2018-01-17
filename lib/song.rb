c3542c7b32550ac7a0b0ed68a0f325314fe4999e
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-17 16:52:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,33 @@ attr_accessor :name, :artist, :genre
     @@artists.uniq
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+    
+
   def self.genres
     @@genres.uniq
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
 end
